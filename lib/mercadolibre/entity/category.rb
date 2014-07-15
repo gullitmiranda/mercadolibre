b0b8e47100221037271e85ed56ae9b9c0a7b2d21
Gullit Miranda <gullitmiranda@requestdev.com.br>
2014-07-15 05:26:57 -0300

    add :to_hash to:  - category  - category_settions  -

--- lib/mercadolibre/entity/category.rb
+++ lib/mercadolibre/entity/category.rb
@@ -10,14 +10,32 @@ module Mercadolibre
 
       def initialize(attributes={})
         attributes.each do |k, v|
-          if ['path_from_root', 'children_categories'].include?(k.to_s)
-            self.state = v.map { |x| Category.new(x) }
+          v = if ['path_from_root', 'children_categories'].include?(k.to_s)
+            v.map { |x| Category.new(x) }
           elsif k.to_s == 'settings'
-            self.settings = CategorySettings.new(v)
-          else
-            self.send("#{k}=", v) if self.respond_to?(k)
+            CategorySettings.new(v)
+          end || v
+
+          self.send("#{k}=", v) if self.respond_to?(k)
+        end
+      end
+
+      def to_hash
+        hash = {}
+
+        self.class.attr_list.map do |k|
+          value = self.send(k)
+
+          unless value.nil?
+            if['path_from_root', 'children_categories'].include?(k.to_s)
+              value.map! { |x| x.to_hash if x.respond_to? :to_hash }
+            else
+              value = value.to_hash if value.respond_to? :to_hash
+            end
+            hash[k] = value
           end
         end
+        hash
       end
 
       private
